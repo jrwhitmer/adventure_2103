22cbb27bc081851934080c5fc48a0ca8ed5a03b5
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-28 11:21:02 -0400

    Create attr_reader for hiker attributes

--- lib/hiker.rb
+++ lib/hiker.rb
@@ -1,8 +1,11 @@
 class Hiker
 
+  attr_reader :name,
+              :experience_level
+
 def initialize(name, experience_level)
   @name = name
   @experience_level = experience_level
-end 
+end
 
 end
