d19b73f8b91aa6f353671fa8a9e6931cf007b3e8
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-28 11:28:37 -0400

    Create test for trail attr_reader

--- spec/trail_spec.rb
+++ spec/trail_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe Trail do
 
       expect(trail).to be_instance_of(Trail)
     end
+
+    it 'has attributes' do
+      trail = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+
+      expect(trail.name).to eq('Grand Wash')
+      expect(trail.length).to eq('2.2 miles')
+      expect(trail.level).to eq(:easy)
+    end
   end
 end
